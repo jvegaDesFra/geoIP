5092f79374f8504876e0f76b3c025c75cd0a97c5
jvegaDesFra <jose.juan.vega@outlook.com>
2015-10-06 10:10:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,6 @@ function getRealIpAddr()
     {
       $ip=$_SERVER['HTTP_CLIENT_IP'];
     }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-    {
-      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
     else
     {
       $ip=$_SERVER['REMOTE_ADDR'];
