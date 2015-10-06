613f3d0d2271ed1c18541fb16bb4640de5a6bfe0
jvegaDesFra <jose.juan.vega@outlook.com>
2015-10-06 10:09:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-echo $_SERVER['HTTP_CLIENT_IP'];
+echo getRealIpAddr();
 
 echo ip_info("173.252.110.27", "Country"); // United States
 echo ip_info("173.252.110.27", "Country Code"); // US
 echo ip_info("173.252.110.27", "State"); // California
 echo ip_info("173.252.110.27", "City"); // Menlo Park
 echo ip_info("173.252.110.27", "Address"); // Menlo Park, California, United States
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
 function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
     $output = NULL;
     if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
