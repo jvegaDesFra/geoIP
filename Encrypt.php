15d9bf99fef9d7ade4be767b8addc5a725bc90b8
jvegaDesFra <jose.juan.vega@outlook.com>
2016-11-14 18:08:34 -0600

    Create Encrypt.php

---
+++ Encrypt.php
@@ -0,0 +1,15 @@
+<?php 
+$valor = $_GET["valor"];
+echo encriptar($valor);
+function encriptar($cadena){
+    $key='ilumovil';  // Una clave de codificacion, debe usarse la misma para encriptar y desencriptar
+    $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $cadena, MCRYPT_MODE_CBC, md5(md5($key))));
+    return $encrypted; //Devuelve el string encriptado
+ 
+}
+ 
+function desencriptar($cadena){
+     $key='ilumovil';  // Una clave de codificacion, debe usarse la misma para encriptar y desencriptar
+     $decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($cadena), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+    return $decrypted;  //Devuelve el string desencriptado
+}
