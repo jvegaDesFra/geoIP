6711eb2861133331f007d9d6d210fbb29dc5f6ac
jvegaDesFra <jose.juan.vega@outlook.com>
2016-11-14 18:16:09 -0600

    Update Encrypt.php

--- Encrypt.php
+++ Encrypt.php
@@ -1,6 +1,21 @@
 <?php 
+// Esto le dice a PHP que usaremos cadenas UTF-8 hasta el final
+mb_internal_encoding('UTF-8');
+ 
+// Esto le dice a PHP que generaremos cadenas UTF-8
+mb_http_output('UTF-8');
 $valor = $_GET["valor"];
-echo encriptar($valor);
+$tipo = $_GET["tipo"];
+
+if($tipo == 1)
+{    
+    echo encriptar($valor);
+}
+else
+{
+    echo desencriptar($valor);
+}
+
 function encriptar($cadena){
     $key='ilumovil';  // Una clave de codificacion, debe usarse la misma para encriptar y desencriptar
     $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $cadena, MCRYPT_MODE_CBC, md5(md5($key))));
